204fa12b51a105f8afcba170407b59215e60dd71
Davide de Lerma <davide.delerma@sa.catapult.org.uk>
2020-07-16 18:09:18 +0100

    updated restore_rasters script

--- restore_rasters.sh
+++ restore_rasters.sh
@@ -1,2 +1,2 @@
-mkdir -p csvs_drr_data/
-aws --endpoint-url=$S3_URL s3 cp s3://$S3_BUCKET/ /csvs_drr_data --recursive
+mkdir -p /rasters_data/csvs_drr_data/
+aws --endpoint-url=$S3_URL s3 cp s3://$S3_BUCKET/ /rasters_data/csvs_drr_data --recursive

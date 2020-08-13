d0d2f904b3255d74c0171fcb125fb05e8ff641d3
Juan Emilio Zurita <juan.macias@sa.catapult.org.uk>
2020-08-13 13:04:08 +0200

    [CSVS-83] Bug fixing
    Redo changes to restore_rasters.sh and apply those to restore_netcdf.sh

--- restore_netcdf.sh
+++ restore_netcdf.sh
@@ -1,2 +1,5 @@
 mkdir -p /netcdf_data/csvs_climate_data/
-aws --endpoint-url=$S3_URL s3 cp s3://csvs-netcdf/ /netcdf_data/csvs_climate_data/ --recursive
+# Download 30 years average data
+aws --endpoint-url=$S3_URL s3 cp s3://csvs-netcdf/ /netcdf_data/csvs_climate_data/ --recursive --exclude "*" --include "*_30yrAv.nc"
+# Download Monthly data
+aws --endpoint-url=$S3_URL s3 cp s3://csvs-netcdf/ /netcdf_data/csvs_climate_data/ --recursive --exclude "*" --include "*_monthly.nc"

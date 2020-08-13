f52d3096361d7c1db340909e5ae9c5851b71ec98
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-08-13 11:52:27 +0100

    Redo changes to restore_rasters.sh and apply those to restore_netcdf.sh

--- restore_rasters.sh
+++ restore_rasters.sh
@@ -1,5 +1,2 @@
 mkdir -p /rasters_data/csvs_drr_data/
-# Download 30 years average data
-aws --endpoint-url=$S3_URL s3 cp s3://csvs-netcdf/ . --recursive --exclude "*" --include "*_30yrAv.nc"
-# Download Monthly data
-aws --endpoint-url=$S3_URL s3 cp s3://csvs-netcdf/ . --recursive --exclude "*" --include "*_monthly.nc"
+aws --endpoint-url=$S3_URL s3 cp s3://csvs-rasters/ /rasters_data/csvs_drr_data --recursive

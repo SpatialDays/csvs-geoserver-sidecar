aeb69116dcfcd875374800fd4807ffebd580c796
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-08-17 12:43:54 +0100

    Add pr review changes

--- restore_backup.sh
+++ restore_backup.sh
@@ -11,20 +11,28 @@ if [ -z "$GEOSERVER_CRED" ] ; then
 fi
 
 {
-    # Download backup file from S3 bucket
+    curl -f -u $GEOSERVER_CRED -X GET http://geoserver:8080/geoserver/rest/reset -I
+} || {
+    echo 'The provided credentials are not valid.'
+    exit 2
+}
+
+{
     aws --endpoint-url=$S3_URL s3 cp s3://csvs-backups/$BACKUP_FILE .
-    # Start restore process
-    echo Starting restore from backup file $BACKUP_FILE &&
-    mkdir -p /geoserver_data/data/backup_unzipped &&
-    mv $BACKUP_FILE tmp.tgz
-    tar -xvzf tmp.tgz -C /geoserver_data/data/backup_unzipped &&
-    cp -r /geoserver_data/data/backup_unzipped/geoserver_data/data/* /geoserver_data/data/ &&
-    echo copying files from abckup to data folder
-    curl -u $GEOSERVER_CRED -X POST http://geoserver:8080/geoserver/rest/reset -I
-    curl -u $GEOSERVER_CRED -X PUT http://geoserver:8080/geoserver/rest/reload -I
-    echo removing tmp files
-    rm -rf /geoserver_data/data/backup_unzipped
-    echo configuration restored
 } || {
     echo 'Failed trying to download the backup file.'
+    exit 2
 }
+
+# Start restore process
+echo Starting restore from backup file $BACKUP_FILE &&
+mkdir -p /geoserver_data/data/backup_unzipped &&
+mv $BACKUP_FILE tmp.tgz
+tar -xvzf tmp.tgz -C /geoserver_data/data/backup_unzipped &&
+cp -r /geoserver_data/data/backup_unzipped/geoserver_data/data/* /geoserver_data/data/ &&
+echo copying files from abckup to data folder
+curl -u $GEOSERVER_CRED -X POST http://geoserver:8080/geoserver/rest/reset -I
+curl -u $GEOSERVER_CRED -X PUT http://geoserver:8080/geoserver/rest/reload -I
+echo removing tmp files
+rm -rf /geoserver_data/data/backup_unzipped
+echo configuration restored
